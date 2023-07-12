a815c1ff4c6391aa178b6370a81c6bc44420a263
Yan Lima Barbosa <yanbr763@gmail.com>
2023-07-12 06:17:12 -0300

    Navbar Error Fixed

--- portfolio-project/js/app.js
+++ portfolio-project/js/app.js
@@ -1,22 +1,22 @@
-const navbar = document.querySelector("#nav");
-const navBtn = document.querySelector("#nav-btn");
-const closeBtn = document.querySelector("#close-btn");
-const sidebar = document.querySelector("#sidebar");
-const date = document.querySelector("#date");
+const navbar = document.querySelector("#nav")
+const navBtn = document.querySelector("#nav-btn")
+const closeBtn = document.querySelector("#close-btn")
+const sidebar = document.querySelector("#sidebar")
+const date = document.querySelector("#date")
 // add fixed class to navbar
 window.addEventListener("scroll", function () {
   if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
+    navbar.classList.add("navbar-fixed")
   } else {
-    navbar.classList.remove("navbar-fixed");
+    navbar.classList.remove("navbar-fixed")
   }
-});
+})
 // show sidebar
 navBtn.addEventListener("click", function () {
-  sidebar.classList.add("show-sidebar");
-});
+  sidebar.classList.remove("show-sidebar")
+})
 closeBtn.addEventListener("click", function () {
-  sidebar.classList.remove("show-sidebar");
-});
+  sidebar.classList.add("show-sidebar")
+})
 // set year
-date.innerHTML = new Date().getFullYear();
+date.innerHTML = new Date().getFullYear()
